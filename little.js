721a1ab03c11a90c5b6ab6635d9c09ad0181cbce
Brad Cornes <bradlc41@gmail.com>
2015-09-02 21:00:37 +0100

    Add document ready function

--- little.js
+++ little.js
@@ -2,6 +2,62 @@
 
 	'use strict';
 
-	window.little = {};
+	/*----------------------------*\
+		Document Ready
+		https://github.com/jfriend00/docReady
+	\*----------------------------*/
+	var ready,
+	    readyList = [],
+	    readyFired = false,
+	    readyEventHandlersInstalled = false;
+
+	function onready() {
+		if ( !readyFired ) {
+			readyFired = true;
+			for( var i = 0; i < readyList.length; i++ ) {
+				readyList[i].fn.call( window, readyList[i].ctx );
+			}
+			readyList = [];
+		}
+	}
+
+	function readyStateChange() {
+		if( document.readyState === "complete" ) {
+			onready();
+		}
+	}
+
+	ready = function( callback, context ) {
+
+		if( readyFired ) {
+			setTimeout( function() {
+				callback( context );
+			}, 1 );
+			return;
+		} else {
+			readyList.push( {fn: callback, ctx: context} );
+		}
+		if( document.readyState === "complete" || (!document.attachEvent && document.readyState === "interactive") ) {
+
+			setTimeout( onready, 1 );
+
+		} else if( !readyEventHandlersInstalled ) {
+
+			if( document.addEventListener ) {
+				document.addEventListener( "DOMContentLoaded", onready, false );
+				window.addEventListener( "load", onready, false );
+			} else {
+				document.attachEvent( "onreadystatechange", readyStateChange );
+				window.attachEvent( "onload", onready );
+			}
+			readyEventHandlersInstalled = true;
+
+		}
+
+	};
+
+	window.little = {
+		ready: ready
+	};
 
 })(window);
